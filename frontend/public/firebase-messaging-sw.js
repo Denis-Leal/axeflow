c75ebc34cc94e5c7c4bb9c50d568d2f470e22e70
Denis Leal <denis.leal07@gmail.com>
2026-04-11 01:02:52 -0300

    fix: public/firebase-messaging-sw.js

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -4,12 +4,13 @@ importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyDbrC_QQffBttmg0W2POqv41ls6Abb4BIA",//process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: "axeflow-6546a.firebaseapp.com", //process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: "axeflow-6546a", //process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: "axeflow-6546a.firebasestorage.app", //process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: "412400342317", //process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: "1:412400342317:web:1daa7a2f0e801e6bb92fa4", //process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: "G-GNC45K00S9"
 });
 
 const messaging = firebase.messaging();
